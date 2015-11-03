df3b0df4bd61efa530ea2fcafa0b495a73753dbc
Daniel Stone <dan.stone@mixrad.io>
2015-11-03 08:49:58 +0000

    added vars for the gdx colors

--- src/gdx/color.clj
+++ src/gdx/color.clj
@@ -38,6 +38,41 @@
          (.-b gdx-color)
          (.-a gdx-color)))
 
+(def black (gdx-color->map Color/BLACK))
+
+(def blue (gdx-color->map Color/BLUE))
+
+(def clear (gdx-color->map Color/CLEAR))
+
+(def cyan (gdx-color->map Color/CYAN))
+
+(def dark-gray (gdx-color->map Color/DARK_GRAY))
+
+(def gray (gdx-color->map Color/GRAY))
+
+(def green (gdx-color->map Color/GREEN))
+
+(def light-gray (gdx-color->map Color/LIGHT_GRAY))
+
+(def magenta (gdx-color->map Color/MAGENTA))
+
+(def maroon (gdx-color->map Color/MAROON))
+
+(def navy (gdx-color->map Color/NAVY))
+
+(def olive (gdx-color->map Color/OLIVE))
+
+(def orange (gdx-color->map Color/ORANGE))
+
+(def pink (gdx-color->map Color/PINK))
+
+(def purple (gdx-color->map Color/PURPLE))
+
+(def red (gdx-color->map Color/RED))
+
+(def teal (gdx-color->map Color/TEAL))
+
 (def white (gdx-color->map Color/WHITE))
 
 (def yellow (gdx-color->map Color/YELLOW))
+
