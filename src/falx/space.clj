ba41fc79ae08b11342bdaa80613a8431efeef733
Daniel Stone <dan.stone@mixrad.io>
2015-11-25 17:53:30 +0000

    added spacial index and attribute index

--- src/falx/space.clj
+++ src/falx/space.clj
@@ -1,14 +1,13 @@
-(ns falx.space
-  (:import (java.util UUID)))
+(ns falx.space)
 
-(defn disjoc
+(defn- disjoc
   [m k v]
   (let [s (get m k)]
     (if-some [new (not-empty (disj s v))]
       (assoc m k new)
       (dissoc m k))))
 
-(defn disjoc-in
+(defn- disjoc-in
   [m [k & ks] v]
   (if (seq ks)
     (if-some [m2 (not-empty (disjoc-in (get m k) ks v))]
@@ -16,7 +15,7 @@
       (dissoc m k))
     (disjoc m k v)))
 
-(defn dissoc-in
+(defn- dissoc-in
   [m [k & ks]]
   (if (seq ks)
     (if-some [m2 (not-empty (dissoc-in (get m k) ks))]
@@ -24,27 +23,69 @@
       (dissoc m k))
     (dissoc m k)))
 
-(defn space
-  [name]
-  {:id   (str (UUID/randomUUID))
-   :name name
-   :idp {}
-   :pid {}
-   :sorted {}})
+(def ^:private set-conj
+  (fnil conj #{}))
 
-(defn find
+(defn cell
+  [map point]
+  {:map map
+   :point point})
+
+(defn position
+  [map point layer]
+  {:map map
+   :point point
+   :layer layer})
+
+(defn space
+  [layer-sort-map]
+  {:sort-order layer-sort-map})
+
+(defn position->cell
+  [position]
+  (cell (:map position) (:point position)))
+
+(defn cell->position
+  [cell layer]
+  (position (:map cell) (:point cell) layer))
+
+(defn find-position
   [space id]
-  (-> space :idp (get id)))
+  (-> space :id (get id)))
+
+(defn find-cell
+  [space id]
+  (when-some [pos (find-position space id)]
+    (position->cell pos)))
+
+(defn find-in-position
+  [space position]
+  (-> space :position (get position)))
+
+(defn find-in-cell
+  [space cell]
+  (-> space :cell (get cell)))
+
+(defn find-in-map
+  [space map]
+  (-> space :map (get map)))
 
 (defn unput
   [space id]
-  (if-let [current (find space id)]
-    (-> (dissoc-in space [:idp id])
-        (disjoc-in [:pid current] id)
-        (dissoc-in [:sorted (:map current)]))))
+  (if-some [pos (find-position space id)]
+    (let [cell (position->cell pos)]
+      (-> space
+          (dissoc-in [:id id])
+          (disjoc-in [:position position] id)
+          (disjoc-in [:cell cell] id)
+          (disjoc-in [:map (:map cell)] id)))
+    space))
 
 (defn put
-  [space id position])
-
-(defn in-map
-  [space map])
+  [space id position]
+  (let [cell (position->cell position)]
+    (-> (unput space id)
+        (assoc-in [:id id] position)
+        (update-in [:position position] set-conj id)
+        (update-in [:cell cell] set-conj id)
+        (update-in [:map (:map cell)] set-conj id))))
