6b68d42698517b69289d736f8ee697345cc2378e
danstone <dan.stone16321@gmail.com>
2016-01-03 05:21:10 +0000

    default event handlers and describe fn

--- src/falx/event.clj
+++ src/falx/event.clj
@@ -1,6 +1,7 @@
 (ns falx.event
   (:require [clojure.core.async :as async]
-            [clojure.tools.logging :refer [debug]]))
+            [clojure.tools.logging :refer [debug]]
+            [falx.util :as util]))
 
 (defonce event-chan
   (async/chan))
@@ -8,6 +9,36 @@
 (defonce event-pub
   (async/pub event-chan :type (fn [_] (async/buffer 128))))
 
+(defn- unregister
+  [m key]
+  (if-some [event (-> m :key (get key))]
+    (-> (util/dissoc-in m [:event event key])
+        (util/dissoc-in [:key key]))
+    m))
+
+(defn- register
+  [m event-type key f]
+  (-> (unregister m key)
+      (assoc-in [:event event-type key] f)
+      (assoc-in [:key key] event-type)))
+
+(defn get-handlers
+  [m event-type]
+  (-> m :event (get event-type) vals))
+
+(defn handle!
+  [m event]
+  (let [handlers (get-handlers m (:type event))]
+    (run! #(% event) handlers)))
+
+(defonce ^:private handlers
+  (atom {}))
+
+(defn defhandler!
+  [event-type key f]
+  (swap! handlers register event-type key f))
+
 (defn publish!
   [event]
-  (async/>!! event-chan event))
+  (async/>!! event-chan event)
+  (handle! @handlers event))
---
+++ src/falx/game/describe.clj
@@ -0,0 +1,12 @@
+(ns falx.game.describe
+  (:require [falx.event :as event]
+            [falx.state :as state]
+            [falx.game.focus :as focus]))
+
+(event/defhandler!
+  [:event.action :action.hit/describe]
+  ::describe
+  (fn [_]
+    (let [g (state/get-game)
+          ts (focus/get-all g)]
+      (run! println ts))))
