6a9671eb139da3ce219cc191127053a7d56be414
danstone <dan.stone16321@gmail.com>
2016-01-03 02:47:10 +0000

    some docstrings

--- src/falx/world.clj
+++ src/falx/world.clj
@@ -1,36 +1,55 @@
 (ns falx.world
+  "Core functions on the world.
+  The world is simply an indexed collection of things."
   (:require [clojure.set :as set]
             [falx.thing :as thing]
             [falx.util :as util]))
 
 (defn publish-event
+  "Publishes an event that can be reacted to.
+  Returns a new world which will retain the `event` under its
+  `:events` key."
   ([world event]
    (update world :events (fnil conj []) event)))
 
 (defn split-events
+  "Removes events from the world, returns
+  a map {:world, :events} where :events will contain
+  the events that had been published so far."
   [world]
   {:world (dissoc world :events)
    :events (:events world)})
 
+(defn get-things
+  "Returns a seq of all things in the world."
+  [world]
+  (-> world :eav vals))
+
 (defn get-thing
+  "Returns the thing with `id`"
   [world id]
   (-> world :eav (get id)))
 
 (defn get-attribute
+  "Gets a single thing attribute"
   ([world id attribute]
    (get-attribute world id attribute nil))
   ([world id attribute not-found]
    (-> (get-thing world id) (get attribute not-found))))
 
 (defn get-ids-by-value
+  "Returns the ids of things having
+  the attribute, value pair."
   [world attribute value]
   (-> world :ave (get attribute) (get value #{})))
 
 (defn get-things-by-value
+  "Returns the things having the given attribute
+  value pair."
   [world attribute value]
   (map #(get-thing world %) (get-ids-by-value world attribute value)))
 
-(defn remove-attribute*
+(defn- remove-attribute*
   [world id attribute]
   (let [value (get-attribute world id attribute ::not-found)]
     (case value
@@ -40,6 +59,7 @@
           (util/disjoc-in [:ave attribute value] id)))))
 
 (defn remove-attribute
+  "Removes an attribute from a thing."
   [world id attribute]
   (let [value (get-attribute world id attribute ::not-found)]
     (case value
@@ -52,6 +72,7 @@
              :value value})))))
 
 (defn set-attribute
+  "Sets the things attribute to the given valie."
   [world id attribute value]
   (-> (remove-attribute* world id attribute)
       (assoc-in [:eav id attribute] value)
@@ -66,6 +87,7 @@
            :previous-value (get-attribute world id attribute)}))))
 
 (defn remove-thing
+  "Completely removes a thing from the world."
   [world id]
   (let [thing (get-thing world id)
         attributes (keys thing)]
@@ -74,7 +96,7 @@
           {:type :event.thing/removed
            :id id}))))
 
-(defn add-thing*
+(defn- add-thing*
   [world thing]
   {:pre [(some? (:id thing))]}
   (let [id (:id thing)
@@ -89,6 +111,10 @@
       (reduce publish-event g events))))
 
 (defn add-thing
+  "Adds the thing to the world, if it already exists attributes
+  are removed or added as appropriate.
+
+  Each thing must be provided with an `:id`."
   [world thing]
   (let [existing (get-thing world (:id thing))]
     (cond
@@ -105,6 +131,8 @@
       :else world)))
 
 (defn update-thing
+  "Applies the function `f` and any `args` to the thing
+  in the world given by `id`."
   ([world id f]
    (let [thing (get-thing world id)]
      (add-thing world (f thing))))
