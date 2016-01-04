62a107d8b09e2a44e994af3d71e03ce95d61bdc4
Daniel Stone <dan.stone@mixrad.io>
2016-01-04 08:27:36 +0000

    + get-creature fn

--- src/falx/game/focus.clj
+++ src/falx/game/focus.clj
@@ -1,6 +1,8 @@
 (ns falx.game.focus
   (:require [falx.game.camera :as camera]
-            [falx.world :as world]))
+            [falx.world :as world]
+            [falx.thing.creature :as creature]
+            [falx.util :as util]))
 
 (defn get-point
   "Get the focused point"
@@ -14,3 +16,7 @@
         world (:world game)]
     (world/get-things-by-value world :point point)))
 
+(defn get-creature
+  "Returns the focused creature, nil if none"
+  [game]
+  (util/find-first creature/creature? (get-all-things game)))
