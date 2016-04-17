6e4afe223bdf2f8a0a31c737f778c51252b516a9
danstone <dan.stone16321@gmail.com>
2016-04-17 23:32:31 +0100

    rebuilt api around update/add/delete

--- src/falx/db.clj
+++ src/falx/db.clj
@@ -29,138 +29,77 @@
 
 (defn get-entity
   "Returns the entity given by the `id`."
-  [g id]
-  (-> g :eav (get id)))
+  [db id]
+  (-> db :eav (get id)))
 
 (defn exists?
   "Returns true if the entity exists."
-  [g id]
-  (contains? (:eav g) id))
-
-(defn get-attr
-  "Returns the attribute `k` of the entity given by `id`."
-  ([g id k]
-   (-> g :eav (get id) (get k)))
-  ([g id k not-found]
-   (-> g :eav (get id) (get k not-found))))
-
-(defn has-attr?
-  "Returns whether the entity has the attribute `k`."
-  [g id k]
-  (-> g :eav (get id) (contains? k)))
-
-(defn ihaving
-  "Returns all entity ids having the attribute(s) `k` & `ks`."
-  ([g k]
-   (-> g :ae (get k)))
-  ([g k & ks]
-   (reduce #(set/intersection %1 (ihaving g %2)) #{} (cons k ks))))
-
-(defn having
-  "Returns the entitys having the attribute(s) `k` & `ks`."
-  ([g k]
-   (map #(get-entity g %) (ihaving g k)))
-  ([g k & ks]
-   (reduce #(set/intersection %1 (having g %2)) #{} (cons k ks))))
+  [db id]
+  (contains? (:eav db) id))
 
 (defn iquery
   "Returns all the entity ids having the attribute `k` with the value `v`.
   If multiple pairs (or a map) is supplied, intersects all entity ids that meet each kv pair."
-  ([g m]
-   (reduce-kv #(set/intersection %1 (iquery g %2 %3)) #{} m))
-  ([g k v]
-   (-> g :ave (get k) (get v #{})))
-  ([g k v & kvs]
-   (iquery g (into {k v} (partition 2 kvs)))))
+  ([db m]
+   (reduce-kv #(set/intersection %1 (iquery db %2 %3)) #{} m))
+  ([db k v]
+   (-> db :ave (get k) (get v #{})))
+  ([db k v & kvs]
+   (iquery db (into {k v} (partition 2 kvs)))))
 
 (defn query
   "Returns all entities having the attribute `k` with the value `v`.
   If multiple pairs (or a map) is supplied, intersects all entity ids that meet each kv pair."
-  ([g m]
-   (map #(get-entity g %) (iquery g m)))
-  ([g k v]
-   (map #(get-entity g %) (iquery g k v)))
-  ([g k v & kvs]
-   (query g (into {k v} (partition 2 kvs)))))
-
-(defn rem-attr
-  "Removes the attribute `k` from the entity given by `id`."
-  ([g id k]
-   (let [v (get-attr g id k ::not-found)]
-     (if (identical? ::not-found v)
-       g
-       (-> g
-           (dissoc-in [:eav id k])
-           (disjoc-in [:ae k] id)
-           (disjoc-in [:ave k v] id)))))
-  ([g id k & ks]
-   (reduce #(rem-attr %1 id %2) g (cons k ks))))
-
-(defn set-attr
-  "Sets the attribute `k` to `v` on the entity given by `id`."
-  ([g id k v]
-   (if (= v (get-attr g id k ::not-found))
-     g
-     (-> g
-         (rem-attr id k)
-         (assoc-in [:eav id k] v)
-         (update-in [:ae k] set-conj id)
-         (update-in [:ave k v] set-conj id))))
-  ([g id k v & kvs]
-   (->> (cons [k v] (partition 2 kvs))
-        (reduce #(set-attr %1 id (first %2) (second %2)) g))))
-
-(defn update-attr
-  "Applies the function `f` and any `args` to the attribute `k` on the entity given by `id`."
-  ([g id k f]
-   (let [v (get-attr g id k)]
-     (set-attr g id k (f v))))
-  ([g id k f & args]
-   (update-attr g id k #(apply f % args))))
-
-(defn rem-entity
-  "Removes the entity given by `id` from the game."
-  [g id]
-  (let [ks (keys (get-entity g id))]
-    (reduce #(rem-attr %1 id %2) g ks)))
-
-(defn merge-entity
-  ([g a]
-   (merge-entity g (:id a) a))
-  ([g id m]
-   (reduce-kv #(set-attr %1 id %2 %3) g m))
-  ([g id m & more]
-   (reduce #(merge-entity %1 id %2) g (cons m more))))
+  ([db m]
+   (map #(get-entity db %) (iquery db m)))
+  ([db k v]
+   (map #(get-entity db %) (iquery db k v)))
+  ([db k v & kvs]
+   (query db (into {k v} (partition 2 kvs)))))
 
 (defn add-entity
-  ([g a]
-   (add-entity g (or (:id a) (inc (:max-id g -1))) a))
-  ([g id m]
-   (let [g (if (number? id)
-             (update g :max-id (fnil max 0) id)
-             g)
-         ea (get-entity g id)]
-     (-> (reduce-kv #(if (contains? m %2)
-                      %1
-                      (rem-attr %1 id %2 %3)) g ea)
-         (merge-entity id m)))))
+  ([db entity]
+   (let [id (:id entity)
+         existing (get-entity db id)
+         ave (as-> (:ave db) ave
+                   (reduce-kv (fn [ave k v]
+                                (if (= v (get entity k ::not-found))
+                                  ave
+                                  (disjoc-in ave [k v] id)))
+                              ave existing)
+                   (reduce-kv (fn [ave k v]
+                                (if (= v (get existing k ::not-found))
+                                  ave
+                                  (update-in ave [k v] set-conj id)))
+                              ave entity))]
+     (-> (assoc-in db [:eav id] entity)
+         (assoc :ave ave))))
+  ([db entity & more]
+   (reduce add-entity (add-entity db entity) more)))
 
-(defn add-entity-coll
-  ([g ecoll]
-   (reduce add-entity g ecoll))
-  ([g ecoll & more]
-   (reduce add-entity-coll g (cons ecoll more))))
+(defn delete-entity
+  ([db id]
+   (if-some [existing (get-entity db id)]
+     (let [ave (reduce-kv (fn [ave k v]
+                            (disjoc-in ave [k v] id))
+                          (:ave db)
+                          existing)]
+       (-> (dissoc-in db [:eav id])
+           (assoc :ave ave)))))
+  ([db id & more]
+   (reduce delete-entity (delete-entity db id) more)))
 
 (defn update-entity
-  ([g id f]
-   (if-some [a (get-entity g id)]
-     (add-entity g (f a))
-     g))
-  ([g id f & args]
-   (update-entity g id #(apply f % args))))
+  ([db id f]
+   (if-some [existing (get-entity db id)]
+     (add-entity db (f existing))
+     db))
+  ([db id f & args]
+   (update-entity db id #(apply f % args))))
 
 (defn db
   ([]
-   {})
+   {:eav {}
+    :ave {}})
   ([ecoll]
-   (add-entity-coll (db) ecoll)))
+   (reduce add-entity (db) ecoll)))
