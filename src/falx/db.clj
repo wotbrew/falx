b8a4bed82326d03ef6f10390e5dcc7f8891bff7c
danstone <dan.stone16321@gmail.com>
2016-06-15 19:51:30 +0100

    some arity overloads

--- src/falx/db.clj
+++ src/falx/db.clj
@@ -1,6 +1,7 @@
 (ns falx.db
   "Contains a low-level value indexed eav database."
-  (:require [falx.db.impl :as impl])
+  (:require [falx.db.impl :as impl]
+            [clojure.set :as set])
   (:refer-clojure :exclude [replace assert]))
 
 (defn exists?
@@ -10,52 +11,70 @@
 
 (defn assert
   "Adds (or sets) the value of `k` for the entity to `v`."
-  [db id k v]
-  (if-not (exists? db id)
-    (-> (impl/assert db id ::id id)
-        (recur id k v))
-    (impl/assert db id k v)))
+  ([db id k v]
+   (if-not (exists? db id)
+     (-> (impl/assert db id ::id id)
+         (recur id k v))
+     (impl/assert db id k v)))
+  ([db id k v & kvs]
+   (let [ret (assert db id k v)]
+     (if kvs
+       (if (next kvs)
+         (recur ret id (first kvs) (second kvs) (nnext kvs))
+         (throw (IllegalArgumentException.
+                  "assert expects even number of arguments after db, found odd number")))
+       ret))))
 
 (defn entity
   "Returns a map describing the state of the entity."
   [db id]
   (-> db ::eav (get id)))
 
-(defn- retract*
-  [db id k v]
-  (-> db
-      (impl/disjoc-in [::ave k v] id)
-      (impl/dissoc-in [::eav id k])))
+(defn ids
+  "Returns a seq of ids"
+  [db]
+  (-> db ::eav keys))
 
 (defn retract
   "Removes the value against `k` from the db for `id`.
   If removing the last attribute of an entity, will not remove the entity.
   Does not allow you to remove the :falx.db/id attribute."
-  [db id k]
-  (if (identical? k ::id)
-    db
-    (retract* db id k (get (entity db id) k))))
+  ([db id k]
+   (if (identical? k ::id)
+     (throw (IllegalArgumentException. "Cannot retract :falx.db/id (delete instead)"))
+     (impl/retract db id k (get (entity db id) k))))
+  ([db id k & ks]
+   (reduce #(retract db id %) (retract db id k) ks)))
 
 (defn delete
   "Completely removes the entity from the db."
-  [db id]
-  (let [ks (keys (entity db id))]
-    (reduce delete db ks)))
+  ([db id]
+   (let [ks (keys (entity db id))]
+     (reduce #(impl/retract %1 id %2) db ks)))
+  ([db id & more]
+   (reduce delete (delete db id) more)))
 
 (defn add
   "Adds or merges the m to the db using the key :falx.db/id to determine entity identity."
-  [db m]
-  (let [id (::id m ::not-found)]
-    (if (identical? id ::not-found)
-      db
-      (reduce-kv #(assert %1 id %2 %3) db m))))
+  ([db m]
+   (let [id (::id m ::not-found)]
+     (if (identical? id ::not-found)
+       (throw (IllegalArgumentException. "map `m` needs to contain :falx.db/id key"))
+       (reduce-kv #(assert %1 id %2 %3) db m))))
+  ([db m & more]
+   (reduce add (add db m) more)))
 
 (defn replace
   "Replaces all attributes of the entity with those in `m`. Requires the :falx.db/id key to be present."
-  [db m]
-  (as-> db db
-        (delete db (::id m))
-        (add db m)))
+  ([db m]
+   (let [id (::id m ::not-found)]
+     (if (identical? id ::not-found)
+       (throw (IllegalArgumentException. "map `m` needs to contain :falx.db/id key"))
+       (as-> db db
+             (delete db id)
+             (add db m)))))
+  ([db m & more]
+   (reduce replace (replace db m) more)))
 
 (defn db
   "Creates a new db"
@@ -72,10 +91,28 @@
 
 (defn iquery
   "Queries for entity ids where `k` == `v`."
-  [db k v]
-  (-> db ::ave (get k) (get v) (or #{})))
+  ([db k v]
+   (-> db ::ave (get k) (get v) (or #{})))
+  ([db k v & kvs]
+   (let [x (iquery db k v)]
+     (if (seq kvs)
+       (if (nnext kvs)
+         (set/intersection
+           x
+           (iquery db (first kvs) (second kvs) (nnext kvs)))
+         (throw (IllegalArgumentException. "iquery expects even number of arguments after db, found odd number")))
+       x))))
 
 (defn query
   "Queries for entities where `k` == `v`."
-  [db k v]
-  (map #(entity db %) (iquery db k v)))
+  ([db k v]
+   (map #(entity db %) (iquery db k v)))
+  ([db k v & kvs]
+   (let [x (query db k v)]
+     (if (seq kvs)
+       (if (nnext kvs)
+         (set/intersection
+           x
+           (query db (first kvs) (second kvs) (nnext kvs)))
+         (throw (IllegalArgumentException. "query expects even number of arguments after db, found odd number")))
+       x))))
