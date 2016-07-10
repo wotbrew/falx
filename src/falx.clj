7c391b28a33e5002dd81ce47a27f790e4cec2b66
danstone <dan.stone16321@gmail.com>
2016-07-10 18:53:17 +0100

    drawing components

--- src/falx.clj
+++ src/falx.clj
@@ -2,8 +2,8 @@
   (:require [clj-gdx :as gdx]
             [clojure.tools.logging :refer [error info debug]]
             [clojure.core.async :as async]
-            [falx.scene :as scene]
             [falx.geom :as g]
+            [falx.draw :as draw]
             [falx.sprite :as sprite]))
 
 (def max-fps
@@ -21,34 +21,12 @@
 (def in
   (async/chan))
 
-(def scene-order
-  (->> [::scene/type
-        :fps
-        :mouse]
-       (map-indexed (fn [i x] [x i]))
-       (into {})))
-
-(def scene-comparator
-  #(compare (scene-order %1 0)
-            (scene-order %2 0)))
-
-(def scene
-  (atom
-    (scene/tree
-      (sorted-map-by scene-comparator
-                     :fps (scene/text
-                            {::scene/geom (g/rect 32 32 64 64)
-                             ::scene/text 0})
-                     :mouse (scene/sprite
-                              {::scene/geom (g/point)
-                               ::scene/sprite sprite/mouse-point})))))
-
 (defn render!
   [game]
-  (swap! scene assoc-in [:fps ::scene/text] (gdx/get-fps))
-  (swap! scene assoc-in [:mouse ::scene/geom] (g/point-tuple->geom (:point @gdx/mouse-state)))
-  (scene/draw!
-    @scene))
+  (draw/draw!
+    [(draw/vspacing 16 [(gdx/get-fps) @gdx/mouse-state])
+     (draw/at (draw/img sprite/mouse-point)
+              (g/point-tuple->geom (:point @gdx/mouse-state)))]))
 
 (gdx/defrender
   (let [g @game]
---
+++ src/falx/draw.clj
@@ -0,0 +1,106 @@
+(ns falx.draw
+  (:require [falx.geom :as g]
+            [gdx.color :as color]
+            [clj-gdx :as gdx]
+            [falx.sprite :as sprite])
+  (:import (clojure.lang Sequential IRef)))
+
+(defprotocol IDraw
+  (-drawfn [this geom context]))
+
+(defn- invoke
+  [f]
+  (f))
+
+(extend-protocol IDraw
+  Object
+  (-drawfn [this geom context]
+    (-drawfn (str this) geom context))
+  Sequential
+  (-drawfn [this geom context]
+    (let [fs (mapv #(-drawfn % geom context) this)]
+      (fn []
+        (run! invoke fs))))
+  String
+  (-drawfn [this geom context]
+    (let [{::g/keys [x y w] :or {x 0 y 0 w 9999}} geom
+          font (:font context gdx/default-font)]
+      (fn []
+        (gdx/draw-string! this x y w context font))))
+  nil
+  (-drawfn [this geom context]
+    (-drawfn "nil" geom context)))
+
+(defn drawfn
+  ([drawable]
+   (drawfn drawable (g/point)))
+  ([drawable geom]
+   (drawfn drawable geom {:color color/white
+                          :font gdx/default-font}))
+  ([drawable geom context]
+   (-drawfn drawable geom context)))
+
+(defn draw!
+  [drawable]
+  ((drawfn drawable)))
+
+(defrecord Size [drawable size]
+  IDraw
+  (-drawfn [this geom context]
+    (let [geom' (g/resize geom size)]
+      (drawfn drawable geom' context))))
+
+(defn size
+  [drawable size]
+  (->Size drawable size))
+
+(defrecord Tint [drawable color]
+  IDraw
+  (-drawfn [this geom context]
+    (let [context (merge-with color/mult context {:color (or color color/white)})]
+      (drawfn drawable geom context))))
+
+(defn tint
+  [drawable color]
+  (->Tint drawable color))
+
+(defrecord Image [sprite]
+  IDraw
+  (-drawfn [this geom context]
+    (let [{::g/keys [x y w h] :or {x 0 y 0 w 32 h 32}} geom]
+      (fn []
+        (gdx/draw-sprite! sprite x y w h context)))))
+
+(defn img
+  [sprite]
+  (->Image sprite))
+
+(defrecord At [drawable point]
+  IDraw
+  (-drawfn [this geom context]
+    (let [geom' (g/put geom point)]
+      (drawfn drawable geom' context))))
+
+(defn at
+  [drawable point]
+  (->At drawable point))
+
+(defrecord Relative [drawable point]
+  IDraw
+  (-drawfn [this geom context]
+    (let [geom' (g/add geom point)]
+      (drawfn drawable geom' context))))
+
+(defn relative
+  [drawable point]
+  (->Relative drawable point))
+
+(defn hspacing
+  [x coll]
+  (let [f (fn [i v] (relative v {::g/x (* x i) ::g/y 0}))]
+    (into [] (map-indexed f) coll)))
+
+(defn vspacing
+  [y coll]
+  (let [f (fn [i v] (relative v {::g/x 0 ::g/y (* y i)}))]
+    (into [] (map-indexed f) coll)))
