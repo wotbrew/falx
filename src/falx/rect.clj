bec76a97d224bebeb3b681798418ddedde8ce22e
danstone <dan.stone16321@gmail.com>
2016-07-24 16:05:58 +0100

    factored out some fns to rect

--- src/falx/rect.clj
+++ src/falx/rect.clj
@@ -18,3 +18,21 @@
 (defn h
   [rect]
   (nth rect 3))
+
+(defn shift
+  ([rect pt]
+    (shift (x rect) (y rect) (w rect) (h rect) pt))
+  ([x y w h pt]
+   (let [[x2 y2] pt]
+     (shift x y w h x2 y2)))
+  ([x y w h x2 y2]
+    [(+ x x2) (+ y y2) w h]))
+
+(defn fit
+  ([rect size]
+    (fit (x rect) (y rect) (w rect) (h rect) size))
+  ([x y w h size]
+   (let [[w2 h2] size]
+     (fit x y w h w2 h2)))
+  ([x y w h w2 h2]
+    [x y (min w w2) (min h h2)]))
