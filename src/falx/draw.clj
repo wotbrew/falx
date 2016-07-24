ee2935320943dbdc4391f5e25b054d13f9e2b680
danstone <dan.stone16321@gmail.com>
2016-07-24 14:08:24 +0100

    combinators and deferred drawing

--- src/falx/draw.clj
+++ src/falx/draw.clj
@@ -3,8 +3,20 @@
             [clojure.java.io :as io]
             [gdx.color :as color]))
 
-(defprotocol IDraw
+(defprotocol IDrawImmediate
   (-draw! [this x y w h opts]))
+
+(defprotocol IDraw
+  (-drawfn [this x y w h opts]))
+
+(defn drawfn
+  ([drawable rect]
+   (let [[x y w h] rect]
+     (-drawfn drawable x y w h {})))
+  ([drawable x y w h]
+   (-drawfn drawable x y w h {}))
+  ([drawable x y w h opts]
+   (-drawfn drawable x y w h opts)))
 
 (defn draw!
   ([this rect]
@@ -15,13 +27,123 @@
   ([this x y w h opts]
    (-draw! this x y w h opts)))
 
-(extend-protocol IDraw
+(extend-protocol IDrawImmediate
   nil
   (-draw! [this x y w h opts]
     (gdx/draw-string! "nil" x y w h opts))
   Object
   (-draw! [this x y w h opts]
-    (gdx/draw-string! this x y w opts)))
+    ((-drawfn this x y w h opts)))
+  falx.draw.IDraw
+  (-draw! [this x y w h opts]
+    ((-drawfn this x y w h opts))))
+
+(extend-protocol IDraw
+  nil
+  (-drawfn [this x y w h opts]
+    (fn []
+      (gdx/draw-string! this x y w opts)))
+  Object
+  (-drawfn [this x y w h opts]
+    (fn []
+      (gdx/draw-string! this x y w opts)))
+  falx.draw.IDrawImmediate
+  (-drawfn [this x y w h opts]
+    (fn []
+      (-draw! this x y w h opts))))
+
+;; ====
+;; Combinators
+;; ====
+
+(defn- invoke
+  [f]
+  (f))
+
+(defrecord Stack [drawables]
+  IDraw
+  (-drawfn [this x y w h opts]
+    (let [fs (mapv #(-drawfn % x y w h opts) drawables)]
+      (fn []
+        (run! invoke fs))))
+  IDrawImmediate
+  (-draw! [this x y w h opts]
+    (run! (fn [d] (draw! d x y w h opts)) drawables)))
+
+(defn stack
+  [drawables]
+  (->Stack drawables))
+
+(defrecord At [drawable x y]
+  IDraw
+  (-drawfn [this x2 y2 w h opts]
+    (-drawfn drawable (+ x x2) (+ y y2) w h opts))
+  IDrawImmediate
+  (-draw! [this x2 y2 w h opts]
+    (-draw! drawable (+ x x2) (+ y y2) w h opts)))
+
+(defn at
+  ([drawable pt]
+   (let [[x y] pt]
+     (->At drawable x y)))
+  ([drawable x y]
+   (->At drawable x y)))
+
+(defrecord In [drawable x y w h]
+  IDraw
+  (-drawfn [this x2 y2 w2 h2 opts]
+    (-drawfn drawable (+ x x2) (+ y y2) (+ w w2) (+ h h2) opts))
+  IDrawImmediate
+  (-draw! [this x2 y2 w2 h2 opts]
+    (-draw! drawable (+ x x2) (+ y y2) (+ w w2) (+ h h2) opts)))
+
+(defn in
+  ([drawable rect]
+   (let [[x y w h] rect]
+     (->In drawable x y w h)))
+  ([drawable x y w h]
+   (->In drawable x y w h)))
+
+(defrecord Fit [drawable w h]
+  IDraw
+  (-drawfn [this x y w2 h2 opts]
+    (-drawfn drawable x y (+ w w2) (+ h h2) opts))
+  IDrawImmediate
+  (-draw! [this x y w2 h2 opts]
+    (-draw! drawable x y (+ w w2) (+ h h2) opts)))
+
+(defn fit
+  ([drawable size]
+   (let [[w h] size]
+     (->Fit drawable w h)))
+  ([drawable w h]
+   (->Fit drawable w h)))
+
+(defrecord Rows [h drawables]
+  IDraw
+  (-drawfn [this x y w h2 opts]
+    (let [fs (into []
+                   (map-indexed (fn [i d]
+                                  (-drawfn d x (+ y (* h i)) w h2 opts)))
+                   drawables)]
+      (fn [] (run! invoke fs)))))
+
+(defn rows
+  ([h drawables]
+   (->Rows h drawables)))
+
+(defrecord Cols [w drawables]
+  IDraw
+  (-drawfn [this x y w2 h opts]
+    (let [fs (into []
+                   (map-indexed (fn [i d]
+                                  (-drawfn d (+ x (* w i)) y w2 h opts)))
+                   drawables)]
+      (fn [] (run! invoke fs)))))
+
+(defn cols
+  ([h drawables]
+   (->Cols h drawables)))
 
 ;; ====
 ;; Strings
@@ -37,17 +159,28 @@
 
 (defrecord Text [s font opts]
   IDraw
+  (-drawfn [this x y w h opts2]
+    (let [context (fast-merge opts opts2)]
+      (fn []
+        (gdx/draw-string! s x y w context font))) )
+  IDrawImmediate
   (-draw! [this x y w h opts2]
     (let [context (fast-merge opts opts2)]
       (gdx/draw-string! s x y w context font))))
 
+(defn text?
+  [x]
+  (instance? Text x))
+
 (defn text
   ([s]
-   (->Text s gdx/default-font {}))
+   (text s gdx/default-font))
   ([s font]
-   (->Text s font {}))
+   (text s font {}))
   ([s font opts]
-   (->Text s font opts)))
+   (if (text? s)
+     (assoc s :font font :opts opts)
+     (->Text s font opts))))
 
 (defn text-padding
   [text w h]
@@ -55,16 +188,12 @@
     [(float (/ (- w w1) 2))
      (float (/ (- h h1) 2))]))
 
-(defn text?
-  [x]
-  (instance? Text x))
-
 ;; =====
 ;; Sprites
 ;; =====
 
 (defrecord Sprite [texture rect flip-y?]
-  IDraw
+  IDrawImmediate
   (-draw! [this x y w h opts]
     (gdx/draw-sprite! this x y w h opts)))
 
@@ -83,6 +212,22 @@
         (int (* y 32))
         32
         32))
+
+;; ====
+;; Colors
+;; ====
+
+(defn- dim
+  [color]
+  (color/scale color 0.5))
+
+(def color-selected color/green)
+(def color-selected2 color/yellow)
+(def color-disabled color/gray)
+
+(def color-default (color/scale color/white 0.9))
+(def color-highlight color/white)
+
 
 ;; =====
 ;; Sprites - Misc
@@ -190,6 +335,17 @@
 
 (defrecord Box [thickness opts]
   IDraw
+  (-drawfn [this x y w h opts2]
+    (let [opts (fast-merge opts opts2)
+          t thickness
+          r-t (+ x (- w t))
+          b-t (+ y (- h t))]
+      (fn []
+        (draw! pixel x y w t opts)
+        (draw! pixel r-t y 1 h opts)
+        (draw! pixel x b-t w 1 opts)
+        (draw! pixel x y t h opts))))
+  IDrawImmediate
   (-draw! [this x y w h opts2]
     (let [opts (fast-merge opts opts2)
           t thickness
@@ -213,6 +369,16 @@
 
 (defrecord CustomBox [thickness lopts topts ropts bopts]
   IDraw
+  (-drawfn [this x y w h _]
+    (let [t thickness
+          r-t (+ x (- w t))
+          b-t (+ y (- h t))]
+      (fn []
+        (draw! pixel x y w t topts)
+        (draw! pixel r-t y 1 h ropts)
+        (draw! pixel x b-t w 1 bopts)
+        (draw! pixel x y t h lopts))))
+  IDrawImmediate
   (-draw! [this x y w h _]
     (let [t thickness
           r-t (+ x (- w t))
@@ -222,94 +388,57 @@
       (draw! pixel x b-t w 1 bopts)
       (draw! pixel x y t h lopts))))
 
-;; ====
-;; Colors
-;; ====
-
-(defn- dim
-  [color]
-  (color/scale color 0.5))
-
-(def color-selected color/green)
-(def color-selected2 color/yellow)
-(def color-disabled color/gray)
-
-(def color-default (color/scale color/white 0.9))
-(def color-highlight color/white)
+(defn shaded-box
+  [thickness color]
+  (->CustomBox
+    thickness
+    {:color color}
+    {:color color}
+    {:color (dim color)}
+    {:color (dim color)}))
 
 ;; ====
 ;; Button
 ;; ====
 
 (def button-box-focused
-  (->CustomBox 1
-               {:color color-highlight}
-               {:color color-highlight}
-               {:color (dim color-highlight)}
-               {:color (dim color-highlight)}))
+  (shaded-box 1 color-highlight))
 
 (def button-box-default
-  (->CustomBox 1
-               {:color color-default}
-               {:color color-default}
-               {:color (dim color-default)}
-               {:color (dim color-default)}))
+  (shaded-box 1 color-default))
 
 (def button-box-disabled
-  (->CustomBox 1
-               {:color color-disabled}
-               {:color color-disabled}
-               {:color (dim color-disabled)}
-               {:color (dim color-disabled)}))
+  (shaded-box 1 color-disabled))
 
 (defn button-box
   [state]
   (case state
-    :ui.button/state.focused button-box-focused
-    :ui.button/state.disabled button-box-disabled
+    :falx.ui.button/state.focused button-box-focused
+    :falx.ui.button/state.disabled button-box-disabled
     button-box-default))
 
 (defn button-text-opts
   [state]
   {:color
    (case state
-     :ui.button/state.focused color-highlight
-     :ui.button/state.disabled color-disabled
+     :falx.ui.button/state.focused color-highlight
+     :falx.ui.button/state.disabled color-disabled
      color-default)})
 
 (defrecord Button [text state]
   IDraw
-  (-draw! [this x y w h opts]
-    (let [[xp yp] (text-padding text w h)]
-      (draw! (button-box state) x y w h)
-      (draw! text (+ x xp) (+ y yp) w h (button-text-opts state)))))
+  (-drawfn [this x y w h opts]
+    (let [[xp yp] (text-padding text w h)
+          button-box (button-box state)
+          x+xp (+ x xp)
+          y+yp (+ y yp)
+          text-opts (button-text-opts state)]
+      (fn []
+        (draw! button-box x y w h)
+        (draw! text x+xp y+yp w h text-opts)))))
 
 (defn button
   ([text]
    (button text nil))
   ([text state]
-   (->Button (if (text? text) text (falx.draw/text text)) state)))
-
-;; ===
-;; Anims
-;; ===
-
-(defrecord ModCycleAnim [sprites rate]
-  IDraw
-  (-draw! [this x y w h opts]
-    (when (seq sprites)
-      (let [time (System/currentTimeMillis)
-            i (mod (/ time rate) (dec (count sprites)))]
-        (-draw! (nth sprites i nil) x y w h opts)))))
-
-;; ====
-;; Anims - Decor
-;; ====
-
-(def torch
-  (->ModCycleAnim
-    [torch1
-     torch2
-     torch3
-     torch4]
-    200))
+   (->Button (falx.draw/text text) state)))
